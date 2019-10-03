b880425d7cbbe57e8799580eced08a734aada8e3
Himanshu Malpande <himanshu.malpande@gmail.com>
2019-10-03 15:28:28 +0530

    Added EmailTags

---
+++ src/templating/email_tags.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Agility\Templating;
+
+	trait EmailTags {
+
+		function embedImg($src, $width = -1, $height = -1, $options = []) {
+
+			$attachment = $this->attachments($src);
+			$src = $attachment->url();
+			$options["alt"] = $attachment->name;
+
+			return $this->img($src, $width, $height, $options);
+
+		}
+
+	}
+
+?>
