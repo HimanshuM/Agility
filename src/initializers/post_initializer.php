f35621a979674ad8e8f467cf81519eb0c49991b7
Himanshu Malpande <himanshu.malpande@gmail.com>
2019-10-03 19:00:30 +0530

    Post initializer loads files from /config/initializers and /config/env../<env>.php

--- src/initializers/post_initializer.php
+++ src/initializers/post_initializer.php
@@ -7,27 +7,25 @@ use StringHelpers\Str;
 
 	final class PostInitializer {
 
-		static $initializers = [
-			"data_initializer",
-		];
-
 		static function execute() {
 
-			// foreach (PostInitializer::$initializers as $initializer) {
-
-			// 	if (Configuration::documentRoot()->has("/config/initializers/$initializer.php")) {
-
-			// 		require_once(Configuration::documentRoot()->has("/config/initializers/$initializer.php"));
-			// 		$className = Str::camelCase($initializer);
-			// 		if (class_exists($className)) {
-			// 			(new $className)->configure();
-			// 		}
-
-			// 	}
-
-			// }
 			foreach (Configuration::documentRoot()->children("config/initializers/") as $initializer) {
 				require_once $initializer;
+			}
+
+			static::executeEnvironmentInitializers();
+
+		}
+
+		static function executeEnvironmentInitializers() {
+
+			$envPath = "config/environments/".Configuration::environment();
+			if (Configuration::documentRoot()->has($envPath)) {
+
+				foreach (Configuration::documentRoot()->children($envPath) as $initializer) {
+					require_once $initializer;
+				}
+
 			}
 
 		}
