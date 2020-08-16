c6dd4cf4638922909d29b91c51bae4c0552641ed
Himanshu Malpande <himanshu.malpande@gmail.com>
2020-08-16 14:28:38 +0530

    Bug fix in post params name

--- src/server/request.php
+++ src/server/request.php
@@ -78,7 +78,7 @@ use AttributeHelper\Accessor;
 			if ($this->method != "get") {
 
 				if (empty($this->request->post)) {
-					$this->post = new Arrays(json_decode($this->request->rawContent(), true));
+					$this->postParams = new Arrays(json_decode($this->request->rawContent(), true));
 				}
 
 			}
